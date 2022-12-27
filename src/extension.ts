ffc7c79089557f03668c44318d641106ce1b52b4
Digant C Kasundra <digant.kasundra@lacework.net>
2022-12-27 15:22:37 -0500

    Basic filtering working

--- src/extension.ts
+++ src/extension.ts
@@ -1,99 +1,237 @@
 import * as vscode from "vscode";
+import { Uri } from "vscode";
 import * as fs from "fs";
 import * as path from "path";
-import { config } from "process";
+import { create } from "domain";
+import { join } from "path";
 
 const filters = [
-	"disco",
-	"oms/helm",
+	"disco/",
+	"oms/helm/",
 ];
 
+/// Provides both the filesystem implementation, so we can add filtered view in the file explorer,
+/// and a tree view so we can set up an editable view of globss
 export class FilteredDirectoryProvider
-	implements vscode.TreeDataProvider<DirItem>
-{
-	private _onDidChangeTreeData: vscode.EventEmitter<any> =
-		new vscode.EventEmitter<any>();
-	readonly onDidChangeTreeData: vscode.Event<any> =
-		this._onDidChangeTreeData.event;
+	implements vscode.FileSystemProvider, vscode.TreeDataProvider<Uri> {
 
-	constructor(private name: string | undefined, private workspaceRoot: string | undefined) {
-		this.workspaceRoot = workspaceRoot;
+	constructor() {
+		this.rebuildRootNodes();
 	}
 
-	public refresh(): any {
-		console.debug("Refreshing");
-		this._onDidChangeTreeData.fire(undefined);
-	}
+	/// map of workspace name and root path
+	private rootNodes: Map<string, string> = new Map();
+	private rebuildTime = Date.now();
 
-	getTreeItem(element: DirItem): vscode.TreeItem {
-		return element;
-	}
 
-	getChildren(element?: DirItem): Thenable<DirItem[]> {
-		if (!this.workspaceRoot) {
-			vscode.window.showInformationMessage("No dependency in empty workspace");
-			return Promise.resolve([]);
+	// We rebuild our root nodes by taking a look at our open workspace folders and adding them as top-level
+	// directories under the filtered directories view
+	rebuildRootNodes() {
+		this.rebuildTime = Date.now();
+		this.rootNodes = new Map();
+
+		// if we don't have any workspace folders open at all, we have nothing to do
+		if (!vscode.workspace.workspaceFolders) {
+			return;
 		}
 
-		if (element) {
-			return Promise.resolve(
-				this.traverseTree(
-					element.fullPath
-				)
+		for (const workspaceRoot of vscode.workspace.workspaceFolders) {
+			// we only support local file systems
+			if (workspaceRoot.uri.scheme !== "file") { continue; }
+
+			this.rootNodes.set(
+				workspaceRoot.name, workspaceRoot.uri.path
 			);
-		} else {
-			return Promise.resolve(this.traverseTree(this.workspaceRoot));
 		}
+
+		// if we have 0 roots, we should remove ourselves as well
+		if (this.rootNodes.size === 0) { vscode.workspace.updateWorkspaceFolders(0, vscode.workspace.workspaceFolders.length); }
 	}
 
-	traverseTree(searchpath: string): DirItem[] {
-		console.debug("Traverse tree " + path);
+	/// takes a path that's given and converts it to a file system path based on workspace root
+	/// and also gives us the "subpath" which is the relative path to the file where we should find it
+	/// if it exists under a workspace root folder
+	convertPath(path: string): string[] {
+		if (path[0] === '/') {
+			path = path.substring(1);
+		}
 
-		let items: DirItem[] = [];
+		let segments = path.split('/');
+		if (!this.rootNodes.has(segments[0])) {
+			throw vscode.FileSystemError.FileNotFound("Fildir could not parse path: " + path);
+		}
 
-		let dirs: DirItem[] = [];
-		fs.readdirSync(searchpath, { withFileTypes: true }).forEach((item) => {
-			let fullpath = path.join(searchpath, item.name);
-			let trimmedpath = fullpath.slice(this.workspaceRoot!.length + 1);
+		let subpath = segments.slice(1).join('/');
 
-			// check filters -- maybe this path starts with one of our filters: add it
-			//  -- maybe our path is a prefix match for a filter, in which case, add it so we can traverse
-			let matchesFilter = false;
-			let prefixOfFilter = false;
-			filters.forEach(f => {
-				if (trimmedpath.startsWith(f)) {
-					matchesFilter = true;
-				}
-				if (f.startsWith(trimmedpath)) {
-					prefixOfFilter = true;
-				}
-			});
+		return [this.rootNodes.get(segments[0]) + "/" + subpath, subpath];
+	}
 
+	/// See if our path is a prefix to one of our filters
+	prefixOfFilter(subpath: string): boolean {
+		let prefixOfFilter = false;
+		for (const filter of filters) {
+			if (filter.startsWith(subpath + "/")) {
+				prefixOfFilter = true;
+			}
+		}
+		return prefixOfFilter;
+	}
 
-			if (item.isDirectory()) {
-				if (matchesFilter || prefixOfFilter) { dirs.push(new DirItem(item.name, trimmedpath, fullpath, false)); }
-			} else if (item.isFile()) {
-				if (matchesFilter) { items.push(new DirItem(item.name, trimmedpath, fullpath, true)); }
+	/// check is a subpath is prefixed by one of our filters
+	subpathMatchesFilter(subpath: string): boolean {
+		if (!subpath.endsWith('/')) {
+			subpath = subpath + "/";
+		}
 
-			} else {
-				console.debug("Skipped: " + item.name);
+		let matchesFilter = false;
+		for (const filter of filters) {
+			if (subpath.startsWith(filter)) {
+				matchesFilter = true;
 			}
 
-		});
-
-		items = dirs.concat(items);
-
-		return items;
-	}
-
-	private pathExists(p: string): boolean {
-		try {
-			fs.accessSync(p);
-		} catch (err) {
-			return false;
 		}
-		return true;
+		return matchesFilter;
 	}
+
+	/// when the workspaces change, we want to either move our workspace folder to the end or remove
+	/// it all together if we don't have any other folders. 
+	workspacesChanged(_event: vscode.WorkspaceFoldersChangeEvent) {
+		let folders = vscode.workspace.workspaceFolders;
+
+		// no folders means we have nothing to do b/c we don't want our folder if no
+		// actual folders are mounted
+		if (!folders) {
+			return;
+		}
+
+		// filter out our folder
+		let newOrder: any[] = [];
+		if (folders) {
+			for (const folder of folders) {
+				if (folder.uri.scheme !== "fildir") {
+					newOrder.push({ uri: folder.uri, name: folder.name });
+				}
+			}
+		}
+
+		// if we are left with no folders after filtering out ours, delete them all
+		if (newOrder.length === 0) {
+
+			vscode.workspace.updateWorkspaceFolders(0, folders.length);
+			return;
+		}
+
+		// add out folder at the end
+		newOrder.push({ uri: vscode.Uri.parse('fildir:/'), name: "Filtered View" });
+		vscode.workspace.updateWorkspaceFolders(0, folders.length, ...newOrder);
+	}
+
+
+	private _treeemitter = new vscode.EventEmitter<Uri | undefined | void>();
+	readonly onDidChangeTreeData = this._treeemitter.event;
+	getTreeItem(element: Uri): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return new vscode.TreeItem(element.path);
+	}
+	getChildren(element?: Uri | undefined): vscode.ProviderResult<Uri[]> {
+		return [];
+	}
+
+	/// We use this to tell VSCode to reexamine our root
+	private _onDidChangeFile = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
+	readonly onDidChangeFile: vscode.Event<vscode.FileChangeEvent[]> = this._onDidChangeFile.event;
+
+	watch(uri: vscode.Uri, options: { readonly recursive: boolean; readonly excludes: readonly string[]; }): vscode.Disposable {
+		return new vscode.Disposable(() => { });
+	}
+
+	stat(uri: vscode.Uri): vscode.FileStat | Thenable<vscode.FileStat> {
+		if (uri.path === "/") {
+			return {
+				ctime: this.rebuildTime,
+				mtime: this.rebuildTime,
+				permissions: vscode.FilePermission.Readonly,
+				size: 0,
+				type: vscode.FileType.Directory,
+			};
+		}
+
+		let [path, subpath] = this.convertPath(uri.path);
+
+		return vscode.workspace.fs.stat(Uri.file(path));
+
+	}
+
+	readDirectory(uri: vscode.Uri): [string, vscode.FileType][] | Thenable<[string, vscode.FileType][]> {
+		// if VSCode is asking about our root, let's rebuild it now
+		if (uri.path === "/") {
+			this.rebuildRootNodes();
+			let roots: [string, vscode.FileType][] = [];
+			for (const rootNode of this.rootNodes) {
+				roots.push([rootNode[0], vscode.FileType.Directory]);
+			}
+			return roots;
+		}
+
+		let [path, subpath] = this.convertPath(uri.path);
+
+
+		// if the directory we want to read starts with one of our prefix filters, than we want to display it
+		let matchesFilter = this.subpathMatchesFilter(subpath);
+
+
+		return vscode.workspace.fs.readDirectory(Uri.file(path)).then(finds => {
+			let items: [string, vscode.FileType][] = [];
+			for (const found of finds) {
+
+				if (matchesFilter) {
+					// because our directory matched a filter, we can display all items
+					items.push(found);
+				} else {
+					// our directory itself didn't match a filter, but one of the subdirs might be
+					// a prefix of one of our filters, meaning it is part of the path to get to our
+					// desired filtered directory
+					let testpath = subpath.trim().length > 0 ? subpath + "/" + found[0] : found[0];
+
+					if (found[1] === vscode.FileType.Directory && this.prefixOfFilter(testpath)) {
+						items.push(found);
+					}
+				}
+			}
+
+			return items;
+		});
+	}
+
+	createDirectory(uri: vscode.Uri): void | Thenable<void> {
+		let [path, subpath] = this.convertPath(uri.path);
+		return vscode.workspace.fs.createDirectory(Uri.file(path));
+	}
+
+	readFile(uri: vscode.Uri): Uint8Array | Thenable<Uint8Array> {
+		let [path, subpath] = this.convertPath(uri.path);
+		return vscode.workspace.fs.readFile(Uri.file(path));
+	}
+
+	writeFile(uri: vscode.Uri, content: Uint8Array, options: { readonly create: boolean; readonly overwrite: boolean; }): void | Thenable<void> {
+		let [path, subpath] = this.convertPath(uri.path);
+		return vscode.workspace.fs.writeFile(Uri.file(path), content);
+	}
+
+	delete(uri: vscode.Uri, options: { readonly recursive: boolean; }): void | Thenable<void> {
+		let [path, subpath] = this.convertPath(uri.path);
+		return vscode.workspace.fs.delete(Uri.file(path), options);
+	}
+
+	rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { readonly overwrite: boolean; }): void | Thenable<void> {
+		let [oldpath, oldsubpath] = this.convertPath(oldUri.path);
+		let [newpath, newsubpath] = this.convertPath(newUri.path);
+		return vscode.workspace.fs.rename(Uri.file(oldpath), Uri.file(newpath), options);
+	}
+
+	copy(source: vscode.Uri, destination: vscode.Uri, options: { readonly overwrite: boolean; }): void | Thenable<void> {
+		throw new Error("Method not implemented.");
+	}
+
 
 	public openClickedItem(path: string) {
 		console.debug("Open " + path);
@@ -103,55 +241,34 @@ export class FilteredDirectoryProvider
 	}
 }
 
+export function activate(context: vscode.ExtensionContext) {
+	const filDirProvider = new FilteredDirectoryProvider();
 
-class DirItem extends vscode.TreeItem {
-	constructor(
-		public readonly name: string,
-		public readonly relativePath: string,
-		public readonly fullPath: string,
-		public readonly isFile: boolean,
-	) {
-		let collapsibleState = isFile ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed
-		super(name, collapsibleState);
-		// this.tooltip = `Hello`;
-		this.description = relativePath;
-		this.relativePath = relativePath;
+	context.subscriptions.push(vscode.workspace.registerFileSystemProvider("fildir", filDirProvider, { isCaseSensitive: true }));
+	context.subscriptions.push(vscode.window.registerTreeDataProvider("fildir", filDirProvider));
+	context.subscriptions.push(vscode.commands.registerCommand('open_clicked_item', fullpath => filDirProvider.openClickedItem(fullpath)));
 
-		if (isFile) {
-			this.command = { command: 'open_clicked_item', arguments: [this.fullPath], title: 'Open' };
-		}
-	}
+	let last = vscode.workspace.workspaceFolders?.length || 0;
+	// this should never be the first workspace folder
+	if (last !== 0) { vscode.workspace.updateWorkspaceFolders(last, 0, { uri: vscode.Uri.parse('fildir:/'), name: "Filtered View" }); }
 
-	iconPath = {
-		light: path.join(
-			__filename,
-			"..",
-			"..",
-			"resources",
-			"light",
-			"dependency.svg"
-		),
-		dark: path.join(
-			__filename,
-			"..",
-			"..",
-			"resources",
-			"dark",
-			"dependency.svg"
-		),
-	};
+	vscode.workspace.onDidChangeWorkspaceFolders(filDirProvider.workspacesChanged);
+
 }
 
-export function activate(context: vscode.ExtensionContext) {
-	const rootPath =
-		vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
-			? vscode.workspace.workspaceFolders[0].uri.fsPath
-			: undefined;
-	const filDirProvider = new FilteredDirectoryProvider(vscode.workspace.name, rootPath);
-	vscode.window.registerTreeDataProvider('fildir', filDirProvider);
-	vscode.commands.registerCommand('fildir.refresh', () =>
-		filDirProvider.refresh()
-	);
-	vscode.commands.registerCommand('open_clicked_item', fullpath => filDirProvider.openClickedItem(fullpath));
+export function deactivate(context: vscode.ExtensionContext) {
+
+	let fildirIndex: number | null = null;
+	vscode.workspace.workspaceFolders?.forEach((folder, index) => {
+
+		if (folder.uri.scheme === "fildir") {
+			fildirIndex = index;
+		}
+	});
+
+	if (fildirIndex !== null) {
+
+		vscode.workspace.updateWorkspaceFolders(fildirIndex, 1);
+	}
 }
 
